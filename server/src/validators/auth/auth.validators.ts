96cf9d798e3eb46adf348d28a3eb37925f6a8ffc
Ritamnandy <nandyritam519@gmail.com>
2026-08-07 23:44:42 +0530

    feat: add validation schemas for user registration, login, password reset, and restaurant details

--- server/src/validators/auth/auth.validators.ts
+++ server/src/validators/auth/auth.validators.ts
@@ -2,7 +2,109 @@
 import { z } from "zod";
 
 const registerSchema = z.object( {
+
+    firstName: z
+        .string()
+        .trim()
+        .min( 3, { message: "First name must be at least 3 characters long" } )
+        .max( 50, { message: "First name must be at most 50 characters long" } ),
+    lastName: z
+        .string()
+        .trim()
+        .min( 3, { message: "Last name must be at least 3 characters long" } )
+        .max( 50, { message: "Last name must be at most 50 characters long" } ),
+    email: z
+        .string()
+        .trim()
+        .email( { message: "Invalid email address" } )
+        .max( 100, { message: "Email must be at most 100 characters long" } ),    
+    password: z
+        .string()
+        .trim()
+        .min( 6, { message: "Password must be at least 6 characters long" } )
+        .max( 50, { message: "Password must be at most 50 characters long" } )
+        .regex( /[A-Z]/, { message: "Password must contain at least one uppercase letter" } )
+        .regex( /[a-z]/, { message: "Password must contain at least one lowercase letter" } )
+        .regex( /[0-9]/, { message: "Password must contain at least one digit" } )
+        .regex( /[^A-Za-z0-9]/, { message: "Password must contain at least one special character" } ),
+    phoneNo: z
+        .string()
+        .trim()
+        .regex(/^[0-9]{10}$/, { message: "Phone number must be 10 digits" })
+        .min( 10, { message: "Phone number must be 10 digits" } )
+        .max( 10, { message: "Phone number must be 10 digits" } ),
+
+} )
+
+const loginSchema = z.object( {
+    email: z
+        .string()
+        .trim()
+        .email( { message: "Invalid email address" } )
+        .max( 100, { message: "Email must be at most 100 characters long" } ),    
+    password: z
+        .string()
+        .trim()
+        .min( 6, { message: "Password must be at least 6 characters long" } )
+        .max( 50, { message: "Password must be at most 50 characters long" } )
+        .regex( /[A-Z]/, { message: "Password must contain at least one uppercase letter" } )
+        .regex( /[a-z]/, { message: "Password must contain at least one lowercase letter" } )
+        .regex( /[0-9]/, { message: "Password must contain at least one digit" } )
+        .regex( /[^A-Za-z0-9]/, { message: "Password must contain at least one special character" } ),
+} )
+
+const verificationSchema = z.object( {
+    email: z
+        .string()
+        .trim()
+        .email( { message: "Invalid email address" } )
+        .max( 100, { message: "Email must be at most 100 characters long" } ),  
+    token: z
+        .string()
+        .trim()
+        .max( 6, { message: "Token must be at most 6 characters long" } ),    
+})
+
+
+const forgetPasswordSchema = z.object( {
+    email: z
+        .string()
+        .trim()
+        .email( { message: "Invalid email address" } )
+        .max( 100, { message: "Email must be at most 100 characters long" } ),  
 } )
 
 
-export { registerSchema }
+const resetpasswordSchema = z.object( {
+    email: z
+        .string()
+        .trim()
+        .email( { message: "Invalid email address" } )
+        .max( 100, { message: "Email must be at most 100 characters long" } ),  
+    token: z
+        .string()
+        .trim()
+        .max( 6, { message: "Token must be at most 6 characters long" } ),    
+    password: z
+        .string()
+        .trim()
+        .min( 6, { message: "Password must be at least 6 characters long" } )
+        .max( 50, { message: "Password must be at most 50 characters long" } )
+        .regex( /[A-Z]/, { message: "Password must contain at least one uppercase letter" } )
+        .regex( /[a-z]/, { message: "Password must contain at least one lowercase letter" } )
+        .regex( /[0-9]/, { message: "Password must contain at least one digit" } )
+        .regex( /[^A-Za-z0-9]/, { message: "Password must contain at least one special character" } ),
+})
+
+
+
+
+
+
+
+export
+{
+    registerSchema,
+    loginSchema, verificationSchema,
+    forgetPasswordSchema, resetpasswordSchema
+}
---
+++ server/src/validators/others/restaurnatbody.validators.ts
@@ -0,0 +1,76 @@
+
+
+import  z  from "zod";
+
+
+const restaurantBodySchema = z.object( {
+    userId: z
+        .string()
+        .trim(),
+    name: z
+        .string()
+        .trim()
+        .min( 5, { message: "Name must be at least 3 characters long" } )
+        .max( 100, { message: "Name must be at most 100 characters long" } ),
+    description: z
+        .string()
+        .trim()
+        .min( 10, { message: "Description must be at least 10 characters long" } )
+        .max( 1000, { message: "Description must be at most 1000 characters long" } ),
+    email: z
+        .string()
+        .trim()
+        .email( { message: "Invalid email address" } )
+        .max( 100, { message: "Email must be at most 100 characters long" } ),     
+    phone:z
+        .string()
+        .trim()
+        .regex(/^[0-9]{10}$/, { message: "Phone number must be 10 digits" })
+        .min( 10, { message: "Phone number must be 10 digits" } )
+        .max( 10, { message: "Phone number must be 10 digits" } ),
+    openingTime: z
+        .date( { message: "Opening time is required" } ), 
+    closingTime: z
+        .date( { message: "Closing time is required" } ),
+    isOpen: z
+        .boolean( { message: "Is open must be a boolean type " } ),        
+} )
+
+
+const restaurantAddressBodySchema = z.object( {
+
+    building: z
+        .string()
+        .trim()
+        .optional(),
+    addressLine: z
+        .string()
+        .trim(),    
+    street: z
+        .string()
+        .trim(),
+    city: z
+        .string()
+        .trim(),
+    state: z
+        .string()
+        .trim(),
+    country: z
+        .string()
+        .trim(),
+    pincode: z
+        .string()
+        .trim()
+        .regex(/^[0-9]{6}$/, { message: "Pincode must be 6 digits" })
+        .min( 6, { message: "Pincode must be 6 digits" } )
+        .max( 6, { message: "Pincode must be 6 digits" } ),
+
+
+} )
+
+
+export
+{
+    restaurantBodySchema,
+    restaurantAddressBodySchema
+}
